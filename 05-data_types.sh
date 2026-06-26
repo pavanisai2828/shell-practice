88797911b267f0bb60c45d37386cee1d986f39b2
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 02:01:12 +0530

    second code added

--- 05-data_types.sh
+++ 05-data_types.sh
@@ -8,7 +8,7 @@ sum2=$(($Num2+$Num3))
 echo "Sum is :$sum"
 echo "Sum2 is :$sum2"
 
-Movies=("RRR","VARANASI","Puspha")
+Movies=("RRR" "VARANASI" "Puspha")
 echo "Movies are :${Movies[@]}"
 echo "First movie:${Movies[0]}"
 echo "Seconf movie: ${Movies[1]}"
