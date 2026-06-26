985b452e2d4df24ebe669c4facddd7ce308015d9
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 01:33:46 +0530

    second code added

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,3 +5,11 @@ b=$2
 echo "Hey $1"
 echo "Hey $2"
 echo "Welcome $1 $2"
+
+echo "please enter your username:"
+read user_name # here user_name is a variable
+echo "I am $user_name"
+echo " Please enter you passowrd:"
+read -s PASSWORD
+
+echo "password is $PASSWORD"
