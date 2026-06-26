5a3adf3a499aa8348b0ac55b8ddb4919e35a407f
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 02:32:37 +0530

    second code added

--- 07-installmysql.sh
+++ 07-installmysql.sh
@@ -13,7 +13,7 @@ if [ $? -eq 0 ]; then
     echo "mysql is already installed... skipping"
 else
     echo "installing mysql"
-    dnf install mysql -y >2
+    dnf installggggg mysql -y >2
     if [ $? -eq 0 ]; then
         echo "success"
     else
