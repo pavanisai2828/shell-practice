42dea00102fb4ee3324113090422e8b1c769995a
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 02:16:34 +0530

    second code added

--- 06-Conditions.sh
+++ 06-Conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 Number=$1
 
-if [$Number -gt 0]; then
+if [ $Number -gt 0 ]; then
     echo "number is greater than 0"
-elif [$Number -eq 0]; then 
+elif [ $Number -eq 0 ]; then 
     echo "number is equal to 0"
-elif [$Number -lt 0]; then
+elif [ $Number -lt 0 ]; then
     echo "number is lessthan 0"
 else
     echo "not a number"
