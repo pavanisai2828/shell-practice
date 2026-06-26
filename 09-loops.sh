1299f6dcad7d5c4ac364f7b18a349b1afd690cc0
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 03:13:55 +0530

    second code added

--- 09-loops.sh
+++ 09-loops.sh
@@ -3,3 +3,44 @@ for number in {1..100}
 do
     echo $number
 done
+
+
+LOGS_DIR=/home/ec2-user/shell-logs
+LOG_FILE="$LOGS_DIR/$0.log"
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+    echo "Please switch to root"
+    exit 1
+fi
+
+
+
+CHECK_INSTALL(){
+    dnf list installed $1 &>> $LOG_FILE
+    if [ $? -eq 0 ];then
+        echo " already installed skipping"
+    else
+        echo "installing"
+        dnf install $1
+        VALIDATE $?
+    fi
+
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ];then
+        echo " installation success"
+    else
+        echo "installation failed"
+    fi
+
+}
+
+for package in $@
+do  
+    echo "package name : $package"
+    CHECK_INSTALL $package
+done
+
