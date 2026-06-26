b128cc8d4a62209e617d99785665edc4423a01d0
pavanisai2828 <pavani.sai1313@gmailcom>
2026-06-27 03:03:58 +0530

    second code added

--- 08-install_using_functions.sh
+++ 08-install_using_functions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LOGS_DIR=/home/ec2-user/shell-logs
+LOG_FILE="$LOGS_DIR/$0.log"
 
 USER=$(id -u)
 
@@ -7,8 +9,10 @@ if [ $USER -ne 0 ]; then
     exit 1
 fi
 
+
+
 CHECK_INSTALL(){
-    dnf list installed $1
+    dnf list installed $1 &>> $LOG_FILE
     if [ $? -eq 0 ];then
         echo " already installed skipping"
     else
